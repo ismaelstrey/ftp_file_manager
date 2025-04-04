5b7fe36ce4500d3689b9a922ca4a105e9659ad60
ismael strey pereira <ismaelstrey@hotmail.com>
2025-04-04 08:41:59 -0300

    chore: update prisma to v6.5.0 and related dependencies
    This commit updates the Prisma package from v6.4.1 to v6.5.0, including all related dependencies such as @prisma/config, @prisma/engines, and esbuild. The changes ensure compatibility with the latest features and improvements in Prisma.

---
+++ prisma/migrations/20250403190435_create_table_commando/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Comando" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "command" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "olt" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
